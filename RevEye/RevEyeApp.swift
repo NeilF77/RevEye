d711441733f35d4e166e212585d51d674b75de29
NeilF77 <c22405604@mytudublin.ie>
2025-11-10 16:50:07 +0000

    Initial Commit

---
+++ RevEye/RevEyeApp.swift
@@ -0,0 +1,32 @@
+//
+//  RevEyeApp.swift
+//  RevEye
+//
+//  Created by user on 10/11/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RevEyeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RevEyeTests/RevEyeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RevEyeTests.swift
+//  RevEyeTests
+//
+//  Created by user on 10/11/2025.
+//
+
+import Testing
+@testable import RevEye
+
+struct RevEyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
